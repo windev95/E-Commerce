23d2689fa354f1bf579b1825eead311b2ca1f633
WIND-PC\BoyIt <hi.qtoan@gmail.com>
2017-05-23 21:27:55 +0700

    Khởi tạo E-Commerce

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ecommerce/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ecommerce/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/ecommerce/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/ecommerce/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/ecommerce/image/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/ecommerce/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/ecommerce/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/ecommerce/system/config/');
+define('DIR_CACHE', 'D:/xampp/htdocs/ecommerce/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/ecommerce/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/ecommerce/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/ecommerce/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/ecommerce/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ecommerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
